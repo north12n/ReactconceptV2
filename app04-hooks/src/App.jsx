b8f6a0f0fbc8b3824443b773b77f19e424a273b4
north12n <952547north@gmail.com>
2024-07-13 11:24:39 +0700

    app04-event onclick

--- app04-hooks/src/App.jsx
+++ app04-hooks/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import "./App.css";
 import ProductList from "./components/ProductList";
-export default function App() {
-  return (
-    <div>
-      <ProductList />
-    </div>
-  );
+
+function App() {
+  
+  return <div>
+    <h1>Product List</h1>
+    <ProductList/>
+  </div>;
 }
+
+export default App;
